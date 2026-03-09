95c744ffd402cb734e3a7565bed7d5864c574e1e
Neha Kumari <your-email@gmail.com>
2026-03-09 15:40:56 +0530

    chalna

---
+++ dekh.cpp
@@ -0,0 +1,45 @@
+//init sai hum .git init file ....directly push code to git hub karke aata hai jisse vo server sai connect ho jata hai 
+//command line interface use karte hai git ke liye
+//git hub 
+//git 
+//stacking area  
+//git maintain ther log file 
+//comment ....local repo in ur device ...so when we commet then it changes to local file repo ...then we push to git hub server ...to connect local repo to git hub server we use git remote add origin url of git hub repo
+
+//push.... directly to git hub server without using command line interface
+//open source of code 
+//redme file ...to give information about project
+//git clone ...to copy code from git hub server to local repo in ur device
+//branch banana 
+//pull request ...to merge code from one branch to another branch
+//mkdir project_name to create the repo 
+//git init to initialize the git repo in that folder
+
+
+//in serch bar type terminal ..then type 
+
+//mkdir project_name
+//cd project_name   
+//git init
+
+
+
+
+
+// PS C:\Users\Lenovo> git clone https://github.com/Neha1805kumari/Testing_repo.git
+// Cloning into 'Testing_repo'...
+// remote: Enumerating objects: 3, done.
+// remote: Counting objects: 100% (3/3), done.
+// remote: Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+// Receiving objects: 100% (3/3), done.
+// PS C:\Users\Lenovo> cd Testing_repo
+// PS C:\Users\Lenovo\Testing_repo> code .
+// PS C:\Users\Lenovo\Testing_repo> 
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Dekhte hai hot ahi ya nahi "<<endl;
+    return 0;
+}
